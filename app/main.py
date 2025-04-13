b3535667910d4f3e1f073c32f642cec3b6c1c536
PavelU_Home <pahan174@gmail.com>
2025-04-13 13:06:22 +0500

    fix: Исправил ошибку в ендпоинте items2

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items2/{item_id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "a": q}
 
 @app.get("/items3/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
